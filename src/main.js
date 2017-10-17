f9a8450ffeb9c7f01080ed81474544b35a52642f
Mesi Rendon <amrendonsa@unal.edu.co>
2017-10-16 23:55:04 -0500

    Adapting VueJS to read contracts

--- src/main.js
+++ src/main.js
@@ -3,13 +3,25 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Web3 from 'web3'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+window.addEventListener('load', () => {
+  if (typeof web3 !== 'undefined') {
+    console.log('Web3 injected browser: OK.')
+    window.web3 = new Web3(window.web3.currentProvider)
+  } else {
+    console.log('Web3 injected browser: Fail. You should consider trying MetaMask.')
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+  }
+
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
+  })
 })
+

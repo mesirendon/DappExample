8efa88a6ebeb44ebaeba372f413538031664e3b4
Mesi Rendon <amrendonsa@unal.edu.co>
2017-10-17 13:09:24 -0500

    Vue Added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>
+      Name
+    </h2>
+    <p>{{ name }}</p>
+    <button type="button" @click="getName">
+      Get Name
+    </button>
   </div>
 </template>
 
 <script>
+import Person from '@/contracts/person'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Dapp Example'
+      msg: 'Dapp Example',
+      name: 'Pending...'
+    }
+  },
+  beforeCreate () {
+    Person.init()
+  },
+  methods: {
+    setName () {
+      Person.setName(this.name).then(console.log)
+    },
+    getName () {
+      Person.getName().then(name => {
+        this.name = name
+      })
     }
   }
 }
